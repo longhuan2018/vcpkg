4906db0ed0740c70aa62fb5716d9032b3ee72c1e
Long Huan <8551701+longhuan2018@users.noreply.github.com>
2020-10-09 17:17:12 +0800

    When the output directory does not exist, an error message is displayed

--- scripts/buildsystems/msbuild/applocal.ps1
+++ scripts/buildsystems/msbuild/applocal.ps1
@@ -42,7 +42,7 @@ try
         if (Test-Path "$outputDir\$_"){
             $baseTargetBinaryDir = $outputDir
         } else {
-            Write-Verbose "  $outputDir: not found"
+            Write-Error "  $outputDir: not found"
             return
         }
     }
